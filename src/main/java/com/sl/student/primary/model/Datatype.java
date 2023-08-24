30d690f442234d645a4bfacd79139e80d42c5fa9
dinesh1717 <dineshnangare1717@gmail.com>
2023-08-24 10:54:06 +0530

    enum added in src

---
+++ src/main/java/com/sl/student/primary/model/Datatype.java
@@ -0,0 +1,23 @@
+package com.sl.student.primary.model;
+
+public enum Datatype {
+    STR(String.class),
+    INT(Integer.class),
+    DOU(Double.class);
+
+    private Class<?> clazz;
+
+    Datatype(Class<?> clazz) {
+        this.clazz = clazz;
+    }
+
+    public Class<?> getClazz() {
+        return clazz;
+    }
+}
+
+
+
+
+
+
